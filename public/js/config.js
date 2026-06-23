97dceb8c37efb3903ff4b7a21963cea2997711b6
2023030483-sudo <2023030483@upsin.edu.mx>
2026-06-22 18:53:13 -0700

    Cambiar diseño del inicio de sesión

--- public/js/config.js
+++ public/js/config.js
@@ -1,3 +1,3 @@
-export const SUPABASE_URL = 'https://oeynlmlijfaqmqcxbchm.supabase.co';
-export const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9leW5sbWxpamZhcW1xY3hiY2htIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA2OTMzNDgsImV4cCI6MjA5NjI2OTM0OH0.rsVfwzlzscQ2sJgQfuAKKTUsZC9LUgp-n63u5IL1EnU';
+export const SUPABASE_URL = 'https://gcvgyrhitdqebkcqabpm.supabase.co';
+export const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdjdmd5cmhpdGRxZWJrY3FhYnBtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIwNzc4OTAsImV4cCI6MjA5NzY1Mzg5MH0.EfcDN1gUpFFRgU_hQzKAEgtktqFyORXF5iTabbQ0_iQ';
 export const ANALYZE_FUNCTION_PATH = '/.netlify/functions/analizar-ticket';
